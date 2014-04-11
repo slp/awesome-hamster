9b43d201e9d42e96a9c984cdf595c67d37d9c15f
Skobara Arthur <skobara.arthur@gmail.com>
2014-04-11 21:20:00 +0700

    Adding fact through dbus

--- awehamster-gui.py
+++ awehamster-gui.py
@@ -6,13 +6,13 @@ import gtk
 class AwesomeHamsterGui():
     def __init__(self):
         self.activitiesList = [ ]
-        
+
         bus = dbus.SessionBus()
 
         proxyHamster = bus.get_object('org.gnome.Hamster', '/org/gnome/Hamster')
-        ifaceHamster = dbus.Interface(proxyHamster, 'org.gnome.Hamster')
+        self.ifaceHamster = dbus.Interface(proxyHamster, 'org.gnome.Hamster')
 
-        activities = ifaceHamster.GetActivities('')
+        activities = self.ifaceHamster.GetActivities('')
 
         for act in activities:
             if act[1] != '':
@@ -20,18 +20,17 @@ class AwesomeHamsterGui():
             else:
                 self.activitiesList.append(act[0])
 
-                
+
     def _match_anywhere(self, completion, entrystr, iter, data):
         modelstr = completion.get_model()[iter][0]
         return entrystr in modelstr
 
-    
+
     def _on_entry_activate(self, entry):
         text = entry.get_text()
         if text != '':
-            os.system('hamster-cli start ' + text)
+            self.ifaceHamster.AddFact(text, 0, 0, False)
             self.dialog.destroy()
-        
 
     def run(self):
         listStore = gtk.ListStore(str)
@@ -53,7 +52,7 @@ class AwesomeHamsterGui():
         entry.set_completion(entryCompletion)
         entry.set_width_chars(maxLen + 5)
         entry.connect("activate", self._on_entry_activate)
-        
+
         self.dialog = gtk.Dialog("New activity",
                                  None,
                                  gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
@@ -67,6 +66,6 @@ class AwesomeHamsterGui():
         hBox.show()
         self.dialog.set_position(gtk.WIN_POS_CENTER_ALWAYS)
         self.dialog.run()
-        
+
 ahgui = AwesomeHamsterGui()
 ahgui.run()
